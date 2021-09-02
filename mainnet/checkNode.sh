8aa11cca865358f5bec11ba55cb05249686d2774
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2021-09-02 20:41:33 +0200

    initial commit
    moved from lbs-cardano to separate repository

---
+++ mainnet/checkNode.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+export CARDANO_NODE_PATH=/home/cardano
+export CARDANO_NODE_SOCKET_PATH=$CARDANO_NODE_PATH/node.socket
+export PATH=$PATH:/usr/share/cardano
+
+systemctl status cardano-node | cat
+cardano-cli --version
+
+online=1
+cardano-cli query tip --mainnet || online=0
+if [[ $online -eq 1 ]]
+then
+  json=`cardano-cli query tip --mainnet`
+  echo $json > $CARDANO_NODE_PATH/logs/current_tip.log
+fi
+
+echo
+echo
+
+free -mh
