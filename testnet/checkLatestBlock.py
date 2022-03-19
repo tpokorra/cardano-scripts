874ea24dae7ee608dc8f69e0e36c2184b227b7c4
Timotheus Pokorra <timotheus.pokorra@solidcharity.com>
2022-03-19 16:17:45 +0100

    use blockfrost.io to check if your node is uptodate

--- testnet/checkLatestBlock.py
+++ testnet/checkLatestBlock.py
@@ -22,19 +22,21 @@ print("  epoch: {0}".format(mynode['epoch']))
 print("  block: {0}".format(mynode['block']))
 print("  sync:  {0}".format(mynode['syncProgress']))
 
-response = requests.get("https://explorer-api.testnet.dandelion.link/api/blocks/pages")
-
+# get your free API key at https://blockfrost.io/auth/signin
+# see https://developers.cardano.org/docs/get-started/blockfrost/
+APIKey="topsecret"
+url="https://cardano-testnet.blockfrost.io/api/v0/blocks/latest"
+header = {'project_id': APIKey}
+response = requests.get(url, headers=header)
 if response.status_code == 200:
-  check = response.json()['Right'][1][0]
-  print("check node:")
-  print("  epoch: {0}".format(check['cbeEpoch']))
-  print("  block: {0}".format(check['cbeBlkHeight']))
-  print("  time:  {0}".format(datetime.datetime.utcfromtimestamp(check['cbeTimeIssued']).strftime('%Y-%m-%d %H:%M:%S')))
-
-  if check['cbeEpoch'] == mynode['epoch']:
-    if mynode['block'] - check['cbeBlkHeight'] <= 10:
-        print("my node is uptodate")
-        exit()
+    check = response.json()
+    #print(response.json())
+    print("  epoch: {0}".format(check['epoch']))
+    print("  height: {0}".format(check['height']))
+    if check['epoch'] == mynode['epoch']:
+        if mynode['block'] - check['height'] <= 10:
+            print("my node is uptodate")
+            exit()
 
 if float(mynode['syncProgress']) > 99.0 and float(mynode['syncProgress']) <= 100.0:
   print("my node is uptodate")
